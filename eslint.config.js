f1c6540cec16737b00b1e7610bc451f755cba83f
arandomogg <ogstevynn@gmail.com>
2026-09-10 10:08:25 +0100

    chore: add ESLint flat config for TypeScript

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+// @ts-check
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettier from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: ["**/dist/**", "**/node_modules/**", "**/target/**", "**/*.js"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: false,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+  prettier,
+];
